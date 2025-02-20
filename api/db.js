b89ba1810e40b312b0137c2f401a3b7c6c59abb1
leeminje0411 <mj12270411@gmail.com>
2025-02-20 17:32:57 +0900

    s

--- api/db.js
+++ api/db.js
@@ -1,19 +1,36 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,  // 올바른 데이터베이스 이름으로 설정
-    port: process.env.DB_PORT
-});
 
-db.connect((err) => {
-    if (err) {
-        console.error('데이터베이스 연결 실패:', err);
-        return;
-    }
-    console.log('데이터베이스 연결 성공');
-});
+let db;
+
+function handleDisconnect() {
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+    });
+
+    db.connect((err) => {
+        if (err) {
+            console.error('데이터베이스 연결 실패:', err);
+            setTimeout(handleDisconnect, 2000); // 2초 후에 다시 연결 시도
+        } else {
+            console.log('데이터베이스 연결 성공');
+        }
+    });
+
+    db.on('error', (err) => {
+        console.error('데이터베이스 연결 에러:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); // 연결이 끊어지면 다시 연결 시도
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = db;
